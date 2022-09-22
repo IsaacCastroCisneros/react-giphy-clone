fcbc67757f9d68257294bcbdbf3f12de6d207868
isaac <isaaccastrocisneros@gmail.com>
2022-09-21 19:08:24 -0500

    fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React,{ useState,useEffect } from 'react'
+import React,{ useState,useEffect,useRef } from 'react'
 import { useInfiniteQuery } from 'react-query'
 import {useInView} from 'react-intersection-observer'
+import Masonry from 'react-masonry-css'
 import axios from 'axios'
 
 function App() 
@@ -36,34 +37,68 @@ function App()
     return res.data
   }
 
+  const random = data?.pages.map((page) => {
+    return (
+      <React.Component key={page.meta.response_id}>
+        {page.data.map((content) => {
+          return (
+            <div>
+              <img
+                className="w-[100%] mb-[1.5rem]"
+                key={content.id}
+                src={content.images.original.url}
+                alt=""
+              />
+            </div>
+          );
+        })}
+      </React.Component>
+    );
+  }) 
+
+  const breakPoints = 
+  {
+    default: 4,
+    999:3,
+    540:2
+  }
+
   console.log(data)
 
-  return(
+  return (
     <>
       <header>
-         <nav>
-             <input type="text" />
-         </nav>
+        <nav>
+          <input type="text" />
+        </nav>
       </header>
-      <div>
-        {
-          data?.pages.map(page=>
-            {
-              return (
-                <React.Fragment key={page.meta.response_id}>
-                  {
-                    page.data.map((content) => 
-                    {
-                      return <img key={content.id} src={content.images.original.url} alt="" />;
-                    })}
-                </React.Fragment>
-              ); 
-            })
-        }
-      </div>
-      <footer ref={ref}></footer>
+      <main className="w-[72rem] max-w-[100%] mx-auto px-[2rem]">
+        <Masonry
+          breakpointCols={breakPoints}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {data?.pages.map((page) => {
+            return ( 
+                page.data.map((content) => {
+                  return (
+                     <div>
+                       <img
+                         className='block w-[100%]'
+                         key={content.id}
+                         src={content.images.downsized_medium.url}
+                       />
+                     </div>
+                  );
+                })
+            );
+          })
+          }
+        </Masonry>
+      </main>
+      <footer ref={ref}>fff</footer>
     </>
-  )
+  );
 }
 
 export default App
